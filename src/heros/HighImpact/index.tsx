c071f927f020275f3ed79a5d9a2ff8c351522ee7
Third Bracket Ltd <hello@thirdbracket.co.uk>
2026-01-06 07:28:40 +0600

    Update index.tsx

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -17,7 +17,7 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
 
   return (
     <div
-      className="relative -mt-[10.4rem]    flex items-center justify-center  backdrop-blur-sm"
+      className="relative -mt-[10.4rem]    flex items-center justify-center  backdrop-blur-md"
       // data-theme="dark"
     >
       <div className="container pt-16 md:pt-24 mb-8 z-10 relative flex items-center justify-center">
