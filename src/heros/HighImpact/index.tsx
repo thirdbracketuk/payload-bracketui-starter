786f4ccbcced3bad5d8bb00a8524bbbea7265462
Third Bracket Ltd <hello@thirdbracket.co.uk>
2026-01-06 06:08:49 +0600

    Update index.tsx

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -17,10 +17,10 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
 
   return (
     <div
-      className="relative -mt-[10.4rem]  backdrop-blur-lg bg-background/50 flex items-center justify-center"
+      className="relative -mt-[10.4rem]    flex items-center justify-center"
       // data-theme="dark"
     >
-      <div className="container pt-16 md:pt-24 mb-8 z-10 relative flex items-center justify-center ">
+      <div className="container pt-16 md:pt-24 mb-8 z-10 relative flex items-center justify-center backdrop-blur-md bg-background bg-opacity-20">
         <div className="max-w-[36.5rem]  md:text-center">
           {richText && <RichText className="mb-6 " data={richText} enableGutter={false} />}
           {Array.isArray(links) && links.length > 0 && (
@@ -40,7 +40,6 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
         {media && typeof media === 'object' && (
           <Media fill imgClassName="-z-10 object-cover" priority resource={media} />
         )}
-        <div className="absolute pointer-events-none left-0 bottom-0 w-full h-1/2 bg-gradient-to-t from-background to-transparent" />
       </div>
     </div>
   )
